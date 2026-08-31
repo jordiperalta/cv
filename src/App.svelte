77fbb81bfb65134c0e5a47011fda4500ce896461
jordiperalta <jordi.peralta@outlook.com>
2026-08-31 17:52:23 +0200

    Switch order in contact components and adapt styles

--- src/App.svelte
+++ src/App.svelte
@@ -377,32 +377,32 @@
   <div class="contact">
     {#if personalData.email}
       <div>
-        <i class="fa-solid fa-envelope contact-icon" aria-hidden="true"></i>
         <span><a href={`mailto:${personalData.email}`}>{personalData.email}</a></span>
+        <i class="fa-solid fa-envelope contact-icon" aria-hidden="true"></i>
       </div>
     {/if}
     {#if personalData.linkedin}
       <div>
-        <i class="fa-brands fa-linkedin contact-icon" aria-hidden="true"></i>
         <span><a href={`https://${personalData.linkedin}`} target="_blank" rel="noopener noreferrer">{personalData.linkedin}</a></span>
+        <i class="fa-brands fa-linkedin contact-icon" aria-hidden="true"></i>
       </div>
     {/if}
     {#if personalData.whatsapp} 
       <div>
-        <i class="fa-brands fa-whatsapp contact-icon whatsapp-icon" aria-hidden="true"></i>
         <span><a href={`https://${personalData.whatsapp}`} target="_blank" rel="noopener noreferrer">{personalData.whatsapp}</a></span>
+        <i class="fa-brands fa-whatsapp contact-icon whatsapp-icon" aria-hidden="true"></i>
       </div>
     {/if}
     {#if personalData.website}
       <div>
-        <i class="fa-solid fa-earth-americas contact-icon" aria-hidden="true"></i>
         <span><a href={`https://${personalData.website}`} target="_blank" rel="noopener noreferrer">{personalData.website}</a></span>
+        <i class="fa-solid fa-earth-americas contact-icon" aria-hidden="true"></i>
       </div>
     {/if}
     {#if personalData.docker}
       <div>
-        <i class="fa-brands fa-docker contact-icon" aria-hidden="true"></i>
         <span><a href={`https://${personalData.docker}`} target="_blank" rel="noopener noreferrer">{personalData.docker}</a></span>
+        <i class="fa-brands fa-docker contact-icon" aria-hidden="true"></i>
       </div>
     {/if}
   </div>
